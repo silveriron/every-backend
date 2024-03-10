d2343e9e9b86db5ec93c5bf9ebbecb8b10cf83d9
silveriron <kichul7493@gmail.com>
2024-03-10 21:42:53 +0900

    Initial commit

---
+++ src/main/java/com/every/everybackend/EveryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.every.everybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EveryBackendApplication.class, args);
+	}
+
+}
