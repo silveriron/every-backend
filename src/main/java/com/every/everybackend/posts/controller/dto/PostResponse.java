d9230499ce646a50629a8a89229ea97123d50f3b
silveriron <kichul7493@gmail.com>
2024-03-13 19:01:13 +0900

    Fix: PostResponse builder 추가

--- src/main/java/com/every/everybackend/posts/controller/dto/PostResponse.java
+++ src/main/java/com/every/everybackend/posts/controller/dto/PostResponse.java
@@ -1,7 +1,10 @@
 package com.every.everybackend.posts.controller.dto;
 
+import lombok.Builder;
+
 import java.time.LocalDateTime;
 
+@Builder
 public record PostResponse(
         Long id,
         String title,
