482cce03c8be4e6e1f5abfb61c260582b59d3539
silveriron <kichul7493@gmail.com>
2024-03-18 14:13:47 +0900

    Fix: 이메일 검증 추가

--- src/main/java/com/every/everybackend/users/controller/dto/ResetPasswordRequest.java
+++ src/main/java/com/every/everybackend/users/controller/dto/ResetPasswordRequest.java
@@ -1,9 +1,11 @@
 package com.every.everybackend.users.controller.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record ResetPasswordRequest(
         @NotBlank
+        @Email
         String email,
         @NotBlank
         String verifyCode,
--- src/main/java/com/every/everybackend/users/controller/dto/SignupUserRequest.java
+++ src/main/java/com/every/everybackend/users/controller/dto/SignupUserRequest.java
@@ -1,9 +1,11 @@
 package com.every.everybackend.users.controller.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record SignupUserRequest(
         @NotBlank
+        @Email
         String email,
         @NotBlank
         String password,
